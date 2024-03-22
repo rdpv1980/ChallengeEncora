32f6b589a23084c829a1150cc6bd3424bc59e985
Rommer Pascacio Valle <rdpv1980@gmail.com>
2024-03-22 06:13:30 -0500

    se actualiza las dependencias con gradle-groovy

---
+++ src/main/java/com/examen/entrevista/scotiabank/ChallengeEncoraMejorasApplication.java
@@ -0,0 +1,13 @@
+package com.examen.entrevista.scotiabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeEncoraMejorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeEncoraMejorasApplication.class, args);
+	}
+
+}
