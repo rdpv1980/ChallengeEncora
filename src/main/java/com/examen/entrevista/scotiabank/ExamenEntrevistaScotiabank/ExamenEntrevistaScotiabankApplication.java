961c74855a1f43f4a7ffbfa04c623ffaa348152c
Rommer Pascacio Valle <rdpv1980@gmail.com>
2025-03-03 20:10:35 -0500

    Se completo el CRUD

--- src/main/java/com/examen/entrevista/scotiabank/ExamenEntrevistaScotiabank/ExamenEntrevistaScotiabankApplication.java
+++ src/main/java/com/examen/entrevista/scotiabank/ExamenEntrevistaScotiabank/ExamenEntrevistaScotiabankApplication.java
@@ -1,6 +1,6 @@
 package com.examen.entrevista.scotiabank.ExamenEntrevistaScotiabank;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
